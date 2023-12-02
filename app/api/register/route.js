e888ceae6755c59d31ff2d60147dfc8af3a7d331
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2023-12-02 01:42:30 +0000

    knex 잘 되네

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,18 +1,154 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const knex = require("knex")({
+  // We are using PostgreSQL
+  client: "postgres",
+  // Use the `DATABASE_URL` environment variable we provide to connect to the Database
+  // It is included in your Replit environment automatically (no need to set it up)
+  connection: process.env.DATABASE_URL,
 
-export async function POST(request) {
+  // Optionally, you can use connection pools to increase query performance
+  pool: { min: 0, max: 80 },
+});
 
-  const body = await request.json();
-  let user = {
-    userName: body.userName,
-    password: body.password,
-    name: body.name,
-    birth: body.birth,
-    generation: body.generation,
+function html2memberSeqVal(html) {
+  // 여기에서는 가져온 HTML을 파싱하거나 원하는 작업을 수행합니다.
+  const root = parse(html);
+  // 예제: 클래스가 "choice"인 요소를 찾아서 onclick 속성 값 추출
+  var buttonElement = root.querySelector('.choice');
+  var onclickAttributeValue = buttonElement.getAttribute('onclick');
+  var functionName = onclickAttributeValue.replace("resultSelect('", "").replace("')", "");
+
+  return functionName;
+}
+
+function html2sodaeVal(html) {
+  const doc = cheerio.load(html);
+  let sosok = doc('.first').find("dd").text();
+  //console.log(sosok);
+
+  var inputString = sosok;
+  var numbers = inputString.match(/\b\d{1,3}\b/g);
+
+  numbers = numbers.map(Number); // 문자열을 숫자로 변환
+  var [대대, 중대, 소대, 호실, 번] = numbers;
+
+  //console.log(`${대대}대대 ${중대}중대 ${소대}소대 ${호실}호실 ${번}번`);
+
+
+  if (번 < 10) {
+    번 = `0${번}`;
   }
 
+  return `${중대}${소대}${번}`;
 
-  const createUser = await prisma.user.create({ data: user })
 }
-// use `prisma` in your application to read and write data in your DB
+
+export async function POST(request) {
+ // const formData = await request.formData()
+   const body = await request.json();
+
+  // 인터넷 편지 사이트 프로필 가져오기
+  const searchName = body.name;
+  const searchBirth = body.birth;
+
+  const url = `https://www.airforce.mil.kr/user/emailPicViewSameMembers.action?siteId=last2&searchName=${searchName}&searchBirth=${searchBirth}`;
+
+  console.log("훈련병 찾는중...",url)
+
+  try {
+  
+    // const response = await axios.get(url);
+    // const html = response.data;
+    // //console.log(html);
+
+    // let memberSeqVal = html2memberSeqVal(html);
+    // let sodaeVal = html2sodaeVal(html);
+
+    // let data = {
+    //   memberSeqVal: memberSeqVal,
+    //   sodaeVal: sodaeVal
+    // };
+    //  console.log(data)
+
+
+    // 인증 된 유저 회원가입
+   // const body = await request.json();
+    console.log(body)
+    console.log("DB 갑니다!")
+      let user = {
+        username: body.username,
+        password: body.password,
+        name: body.name,
+        birth: body.birth,
+        generation: body.generation,
+     //   memberSeq:memberSeqVal,
+       // sodae: sodaeVal,
+       // connect: true,
+      }
+    console.log("얍!")
+
+
+
+    // // 테이블 만들때 실행
+    // await knex.schema.hasTable('users')
+    // .then((exists) => {
+    //   if(!exists){
+        
+
+    //   }
+    // });
+   
+    await knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('username');
+      table.string('password');
+      table.string('name');
+      table.string('birth');
+      table.string('generation');
+      table.string('memberSeq').nullable();
+      table.string('sodae').nullable();
+      table.boolean('connect').defaultTo(false);
+    });
+
+   
+
+    // add user
+    await knex("users").insert(user);
+
+
+
+    
+    console.log("성공!")
+    return new Response('Hello, Next.js!', {
+      status: 200
+    })
+
+  } catch (e) {
+
+    console.log(e)
+     //return Response.error("해당하는 훈련병이 없거나 편지쓰기 가능 시간이 아닙니다.");
+
+    // 인증 안된 유저 회원가입
+    // const body = await request.json();
+    // let user = {
+    //   userName: body.userName,
+    //   password: body.password,
+    //   name: body.name,
+    //   birth: body.birth,
+    //   generation: body.generation,
+    // }
+
+    // const createUser = await prisma.user.create({ data: user })
+
+  } 
+
+  
+
+
+
+
+
+
+
+
+
+}
---
+++ app/register/page.jsx
@@ -0,0 +1,87 @@
+'use client';
+
+import React, { useRef, useState } from 'react';
+import { useRouter } from 'next/navigation'
+import axios from "axios";
+
+export default function Register() {
+  const router = useRouter();
+
+  const name = useRef("");
+  const generation = useRef("");
+  const year = useRef("");
+  const month = useRef("");
+  const date = useRef("");
+  const [mailLink, setMailLink] = useState("");
+
+
+  function fetchData() {
+
+
+    axios.post('/api/register', {
+      username: "aaaa",
+      password: "pwaaa",
+      name: name.current,
+      birth: `${year.current}${month.current}${date.current}`,
+      generation: generation.current
+    })
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(error) {
+
+        console.log("오류:", error);
+        // setValidate(false);
+      }).finally(function() {
+        // setComplete(true);
+      })
+  }
+
+  const handleSubmit = async () => {
+
+    fetchData();
+
+
+    console.log(router)
+    let link = `https://airforce-mail.vercel.app/mail?generation=${generation.current}&searchName=${name.current}&searchBirth=${year.current}${month.current}${date.current}`;
+    setMailLink(link);
+
+  }
+
+  return (
+    <>
+
+      <h1>링크 만들기!!!!!</h1>
+      <p> 편지 링크를 만듭니다.</p>
+
+      <br />
+      <div>
+        <input minLength="1" name="name" id="name" type="text" placeholder='이름'
+          required style={{ "width": "235px" }}
+          onChange={(e) => { name.current = e.target.value; }}></input>
+        <br />
+        <input minLength="1" name="generation" id="generation" type="text" placeholder='기수'
+          required style={{ "width": "235px" }}
+          onChange={(e) => { generation.current = e.target.value; }}></input>
+        <br />
+        <input minLength="1" name="year" id="year" type="text" placeholder='년도'
+          required style={{ "width": "80px", "margin-right": "10px" }}
+          onChange={(e) => { year.current = e.target.value; }}></input>
+        <input minLength="1" name="month" id="month" type="text" placeholder='월'
+          required style={{ "width": "80px", "margin-right": "10px" }}
+          onChange={(e) => { month.current = e.target.value; }}></input>
+        <input minLength="1" name="date" id="date" type="text" placeholder='일'
+          required style={{ "width": "80px" }}
+          onChange={(e) => { date.current = e.target.value; }}></input>
+        <br />
+        <h1>{mailLink}</h1>
+
+        <button onClick={handleSubmit}>
+          링크 만들기
+        </button>
+      </div>
+
+
+    </>
+  )
+}
