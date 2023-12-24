9495fa14012fb5f31e8ba1a00a4c02e2f6c39f0b
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2023-12-24 10:50:14 +0000

    아이콘 css md-128 global에 만듬

--- app/link/[username]/page.jsx
+++ app/link/[username]/page.jsx
@@ -3,37 +3,31 @@ import styles from "./link.module.css";
 
 export default async function Link({ params }) {
   function copy() {
-    let local="https://airforce-mail--oyc0401.repl.co";
-    let production="https://oyc0401.site";
-    
+    let local = "https://airforce-mail--oyc0401.repl.co";
+    let production = "https://oyc0401.site";
+
+    navigator.clipboard.writeText(`${local}/mail/${params.username}`);
     // navigator.clipboard.writeText(
-    //   `${local}/mail/${params.username}`,
+    //   `${production}/mail/${params.username}`,
     // );
-    navigator.clipboard.writeText(
-      `${production}/mail/${params.username}`,
-    );
-    alert("복사돠었습니다!");
+
+    alert("복사되었습니다!");
   }
 
   return (
     <>
       <div className="screen">
         <div style={{ flex: 151 }}></div>
-        <span className={`material-symbols-outlined ${styles.icon}`}>
-          check_circle
-        </span>
+        <span className={`material-symbols-outlined md-128`}>check_circle</span>
         <div style={{ height: 28 }}></div>
-        <h2 className={styles.title}>링크가 생성되었습니다!</h2>
-        {/* <div>{`http://oyc0401.site/write/${params.username}`}</div> */}
-
+        <h2 className="font-bold text-2xl">링크가 생성되었습니다!</h2>
         <div style={{ flex: 188 }}></div>
-        <h2 className={styles.subtitle}>
+        <h2 className="font-medium text-lg">
           링크를 공유하고
           <br />
           편지를 받으세요!
         </h2>
         <div style={{ height: 35 }}></div>
-        {/* <div id="link">{`http://dsadsadas`}</div> */}
         <button className={"submit"} onClick={copy}>
           링크 복사
         </button>
