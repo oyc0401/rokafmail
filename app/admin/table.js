da2ab968d3dbf9ccd0e1bfc5c931638dddb31d23
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2023-12-24 12:05:51 +0000

    관리자 페이지 구현

---
+++ app/admin/table.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+export function Table(props) {
+  const data = props.data;
+  const columns = Object.keys(data[0]);
+
+console.log(data);
+  function renderCellValue(value) {
+    if (typeof value === 'boolean') {
+      return value ? 'T' : 'F';
+    } else {
+      return value;
+    }
+  }
+  
+  return (
+    <>
+      <table className="w-full border-collapse mt-4">
+        <thead className="bg-gray-200">
+          <tr>
+            {columns.map((column) => (
+              <th key={column} className="border px-4 py-2">
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) => (
+                <td key={colIndex} className="border px-4 py-2">
+                  {renderCellValue(row[column])}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
---
+++ app/admin/users/page.jsx
@@ -0,0 +1,56 @@
+import {Table} from'../table'
+import Link from "next/link";
+
+export default async function Users() {
+  const knex = require("knex")({
+    client: "postgres",
+    connection: process.env.DATABASE_URL,
+    pool: { min: 0, max: 80 },
+  });
+
+  const users = await knex("users");
+  
+  const data = users;
+  knex.destroy();
+  return (
+    <>
+      <div className=" mx-auto p-4">
+       
+        <div className="flex space-x-4 mb-4">
+          <Link
+            href="/admin/users"
+            className="bg-gray-500 border border-gray-300 px-3 py-2 text-white font-bold"
+          >
+            유저
+          </Link>
+          <Link
+            href="/admin/users_queue"
+            className="bg-white border border-gray-300 px-3 py-2"
+          >
+            유저큐
+          </Link>
+          <Link
+            href="/admin/post"
+            className="bg-white border border-gray-300 px-3 py-2"
+          >
+            편지
+          </Link>
+          <Link
+            href="/admin/post_queue"
+            className="bg-white border border-gray-300 px-3 py-2"
+          >
+            편지큐
+          </Link>
+          <Link
+            href="/admin/unconnected_post"
+            className="bg-white border border-gray-300 px-3 py-2"
+          >
+            안보내진편지
+          </Link>
+        </div>
+        <Table data={JSON.parse(JSON.stringify(data))}></Table>
+      </div>
+    </>
+  );
+
+};
