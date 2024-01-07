2a4fd58cf7342476925b64a60ef6778372041296
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-01-07 08:26:35 +0000

    initial주소 rds

--- src/app/(admin)/initialSetting/route.js
+++ src/app/(admin)/initialSetting/route.js
@@ -1,7 +1,17 @@
-
 const knex = require("knex")({
   client: "postgres",
-  connection: process.env.DATABASE_URL,
+  connection:
+    "postgres://postgres:chan0401@rokafmail.cr68iaqe6qwa.ap-northeast-2.rds.amazonaws.com:5432/postgres?sslmode=require",
+  // connection:'postgres://neon:go5SiZ8LGkwu@ep-winter-union-23827944.us-east-2.aws.neon.tech/neondb?sslmode=require&options=project%3Dep-winter-union-23827944',
+  // connection: process.env.DATABASE_URL,
+  // connection: {
+  //   user: "postgres",
+  //   host: "rokafmail.cr68iaqe6qwa.ap-northeast-2.rds.amazonaws.com",
+  //   database: "postgres",
+  //   password: "chan0401",
+  //   port: 5432,
+  // },
+  // ssl: { rejectUnauthorized: false },
   pool: { min: 0, max: 80 },
 });
 export async function GET(request) {
