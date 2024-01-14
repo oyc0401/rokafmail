819e2ba0cdd7cce8be001d35a1bd86a4453208e0
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-01-14 14:13:52 +0000

    prisma 버전 업데이트, jsx 제거

--- src/app/link/[username]/page.jsx
+++
@@ -1,7 +0,0 @@
-import { Client } from "./client";
-export default function LinkPage({ params }) {
-  let domain = process.env.DOMAIN;
-  let url = `https://${domain}/mail/${params.username}`;
-
-  return <Client url={url} />;
-}
