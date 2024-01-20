94b29a9336665effdae6d8cc449bace407a66807
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-01-20 11:47:42 +0000

    cron timezone

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -9,14 +9,21 @@ var cron = require("node-cron");
 export async function POST() {
   console.log("cron 시작!");
   // 매일 8,12,18시 마다
-  cron.schedule("00 */4 * * *", () => {
-    try {
-      console.log("node-cron is executed");
-      run();
-    } catch (e) {
-      console.log("node-cron 중 오류발생", e);
-    }
-  });
+  cron.schedule(
+    "00 */4 * * *",
+    () => {
+      try {
+        console.log("node-cron is executed");
+        run();
+      } catch (e) {
+        console.log("node-cron 중 오류발생", e);
+      }
+    },
+    {
+      scheduled: true,
+      timezone: "Asia/Seoul",
+    },
+  );
 
   return NextResponse.json({ message: "cron 시작" }, { status: 200 });
 }
