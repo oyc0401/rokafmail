7b9599d6c9908b2a91a2af7b1d2a637dc8112318
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-01-23 13:41:53 +0000

    logpage

--- src/app/(admin)/log/page.tsx
+++ src/app/(admin)/log/page.tsx
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 export default function LogPage() {
-  const filePath = path.join(process.cwd(), "logs/2024-01-21.log"); // 파일 경로 생성
+  const filePath = path.join(process.cwd(), "logs/2024-01-23.log"); // 파일 경로 생성
   const byte = fs.readFileSync(filePath, "utf-8");
   //console.log(byte);
 
