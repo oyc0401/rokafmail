560c4a66ca5a09e5806c5b44ecbd23dd27e0c6d4
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-02-17 05:37:14 +0000

    편지 삭제하는것도 로그찍기

--- src/app/mails/[username]/server.ts
+++ src/app/mails/[username]/server.ts
@@ -1,13 +1,17 @@
 "use server";
 
 import { Post } from "src/db";
+import { makeLogger } from "config/winston";
+const logger = makeLogger("DeleteMail");
 
 export async function deletePost(id, password) {
- 
   const post = await Post.findById(id);
-   console.log(id,password,post?.password)
+  console.log(id, password, post?.password);
   if (post && post.password == password) {
     await Post.deleteById(id);
+    logger.info(
+      `${id} 삭제! ${post.title} | ${post.contents} | ${post.name} | ${post.relationship} | ${post.password}`,
+    );
     return true;
   }
   return false;
