d8a0bc9e61d8e0d5fc66871b16eece1f23c15495
oyc0401 <73932179+oyc0401@users.noreply.github.com>
2024-02-19 12:21:41 +0000

    안쓰는 키, 중복확인 로그 지움

--- src/app/api/repeat/initialize/route.ts
+++ src/app/api/repeat/initialize/route.ts
@@ -4,8 +4,6 @@ import {Repeat} from '../repeat';
 export async function POST(request: Request) {
   const { key } = await request.json();
 
-  console.log('key', key)
-  
   if(key != process.env.NEXTAUTH_SECRET){
      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,9 +2,6 @@
 import { User } from "src/db";
 
 export async function duplicateUsername(username) {
-  console.log("아이디 중복확인 중...");
   const duplicate = (await User.countUsername(username)) != 0;
-  console.log("아이디 중복확인 완료!", !duplicate);
-
   return duplicate;
 }
