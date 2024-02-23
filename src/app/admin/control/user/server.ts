2cd1ec31129d95eb456dd5258bd80db78599012b
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-02-23 08:22:46 +0000

    하나씩 유저 인증 하게 하는거 만듬

--- src/app/admin/control/user/server.ts
+++ src/app/admin/control/user/server.ts
@@ -1,7 +1,42 @@
-'use server'
+"use server";
+
+import { verify } from "src/app/api/retry/verifyUserOnce";
+import { User } from "src/db";
+import { makeLogger } from "config/winston";
+import { VerifyStatus } from "src/app/api/retry/verifyUserOnce";
+const logger = makeLogger("verifyUser");
 
 // import {} from'src/app/api/retry/'
 
-export async function UserDoubleCheck(userId:number){
-  
+export async function userDoubleCheck(userId: number) {
+  const user = await User.findById(userId);
+  if (!user) return;
+
+  const { username, name, generation, birth } = user;
+  const status = await verify({ userId, name, generation, birth });
+
+  const userLogForm = `| ${username} (${userId}) ${name} ${birth} ${generation}`;
+  let msg = "";
+  switch (status) {
+    case VerifyStatus.verify:
+      msg = `verify ${userLogForm}`;
+      logger.info(msg);
+      return msg;
+    case VerifyStatus.notfound:
+      msg = `notfound ${userLogForm}`;
+      logger.info(msg);
+      return msg;
+    case VerifyStatus.skip:
+      msg = `skip ${userLogForm}`;
+      logger.info(msg);
+      return msg;
+    case VerifyStatus.unidentify:
+      msg = `unidentify ${userLogForm}`;
+      logger.info(msg);
+      return msg;
+    case VerifyStatus.error:
+      msg = `error ${userLogForm}`;
+      logger.info(msg);
+      return msg;
+  }
 }
