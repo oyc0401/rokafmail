07faa7ed8ad4e268f77603d33debd6d276d78688
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-04-17 06:19:50 +0000

    jest 설치완료

--- src/lib/time/test.ts
+++ src/lib/time/test.ts
@@ -3,7 +3,6 @@ import { getEnter } from "./time";
 describe('time 라이브러리', () => {
   test('850기', () => {
     const a = getEnter(850)
-    console.log(a.format('YYYY-MM-DD hh:mm:ss'))
-    expect(a.format('YYYY-MM-DD hh:mm:ss')).toBe('2023-08-14 00:00:00');
+    expect(a.format('YYYY-MM-DD HH:mm:ss')).toBe('2023-08-14 00:00:00');
   });
 })
