e249d89f95e65672a746fbb5fd7878f97f3a545f
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-05-23 14:59:35 +0000

    feat: 이분탐색으로 기수 추천 기능 개발

--- src/lib/time/recommendDate.ts
+++ src/lib/time/recommendDate.ts
@@ -5,18 +5,25 @@ import dayjs from 'dayjs';
 // 날짜에 따른 추천하는 기수 보여주기
 export function getRecommendGeneration(date: Date): number {
   const targetDate = dayjs(date);
-  const generations = Object.keys(store).map(Number);
+  const generations = Object.keys(store).map(Number); // store의 키는 이미 정렬되어 있다고 가정
 
-  for (let i = generations.length - 1; i >= 0; i--) {
-    const generation = generations[i];
-    const [startDateStr] = store[generation];
-    const startDate = dayjs(startDateStr);
-    const enlistmentThreshold = startDate.subtract(7, 'day');
+  let left = 0;
+  let right = generations.length - 1;
 
-    if (targetDate.isAfter(enlistmentThreshold) || targetDate.isSame(enlistmentThreshold)) {
-      return generation;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const startDate = dayjs(store[generations[mid]][0]).subtract(7, 'day');
+
+    if (startDate.isAfter(targetDate)) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
 
-  return generations[0]; // DB에 있는 첫 번째 기수보다 이전 날짜는 830을 반환
+  if (left === 0) {
+    return generations[0]; // DB에 있는 첫 번째 기수보다 이전 날짜는 첫 번째 기수를 반환
+  }
+
+  return generations[right];
 }
