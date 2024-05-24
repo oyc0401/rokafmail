955b3b3831825d49acdd782ba7ecd229ff387e3d
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-05-24 14:40:01 +0000

    robots 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/mails/",
+      disallow: ["/mail/", "/mails/"]
     },
     sitemap: "https://rokafmail.kr/sitemap.xml",
   };
