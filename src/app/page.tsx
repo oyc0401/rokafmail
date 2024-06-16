d2a8920fcc9ce1ed5cdd06e4a3fbfa044667b5a0
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-06-16 04:56:35 +0000

    feat: 메인화면 ui 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import localFont from 'next/font/local'
 import { Logo } from "public/assets/index";
 import { FitScreen } from "src/components/FitScreen";
+import { NavHeaderHome } from "src/components";
 
 import styles from "./page.module.css";
 import { IsAuthenticated, IsLoading, IsNotAuthenticated } from "src/components";
@@ -21,31 +22,40 @@ export default function Page() {
 
 function TopView() {
   return (
-    <FitScreen>
-      <div className="h-full max-w-3xl mx-auto flex flex-col px-4">
-        <div style={{ flex: 90 }}></div>
-        <div className="p-5">
-          <Image className='h-40 w-40 mx-auto' src={Logo} alt="로고" />
-          <div style={{ paddingTop: 38 }}>
-            <h2 className={`${INKLIPQUID.className} ${styles.titleLogo}`}>
-              하늘인편
-            </h2>
+    <>
+      <NavHeaderHome></NavHeaderHome>
+      <div className="max-w-3xl mx-auto flex flex-col px-4">
+        <div className="pt-8 pb-12">
+          <h1 className='text-2xl font-bold text-left' >
+            <span className="text-primary">하늘인편</span>으로
+            <br />
+            인터넷 편지를 받아보세요
+          </h1>
+          <p className="text-xs text-fontmedium text-left pt-2">
+            하늘인편은 편지를 쉽게 받을 수 있는
+            <br />
+            공군 인터넷편지 서비스입니다
+          </p>
+        </div>
+        <div className="py-4">
+          <div className="p-4 bg-[#FAFAFA] text-sm">
+            <p className="text-medium font-bold pb-3">하늘인편으로 인편 받는법!</p>
+            <p>1. 입대 전 SNS에 하늘인편 링크를 공유한다.</p>
+            <p>2. 친구들이 링크를 보고 편지를 보낸다.</p>
+            <p>3. 훈련소에서 인터넷편지를 받는다.</p>
           </div>
         </div>
-
-        <div style={{ flex: 130 }}></div>
-
-        <div className="pt-1 pb-8 w-full">
-          <h1 className={'text-xl'}>
-            입대전에 인편 링크를
+        <div className="pt-8 pb-6 w-full">
+          <p className="font-light text-base">
+            이번 기수인 858기 중 646명이
             <br />
-            만들고 공유하세요!
-          </h1>
+            하늘인편을 통해 편지를 받았습니다!
+          </p>
         </div>
-        <Link className="submit" href={{ pathname: "/register" }}>
-          편지함 만들기
+        <Link className="submit max-w-2xl mx-auto" href={{ pathname: "/register" }}>
+          편지함 생성하기
         </Link>
-        <div className="pt-2.5 pb-5 w-full">
+        <div className="pt-4 pb-5 w-full">
           <div
             style={{
               display: "flex",
@@ -53,82 +63,76 @@ function TopView() {
               justifyContent: "center",
             }}
           >
-            <a href="/profile" className={`text-base underline hover:text-darkaccent`}>
-              <IsAuthenticated>내 정보</IsAuthenticated>
-              <IsNotAuthenticated>로그인</IsNotAuthenticated>
-              <IsLoading>로그인</IsLoading>
-            </a>
+            <IsAuthenticated>
+              <a href="/profile" className="text-base underline hover:text-darkaccent">내 정보</a>
+            </IsAuthenticated>
+            <IsNotAuthenticated>
+              <span className="pr-2 text-base">이미 가입하셨으면?</span>
+              <a href="/profile" className="text-base underline hover:text-darkaccent">로그인</a>
+            </IsNotAuthenticated>
+            <IsLoading>
+              <span className="pr-2 text-base"> 이미 가입하셨으면?</span>
+              <a href="/profile" className="text-base underline hover:text-darkaccent">로그인</a>
+            </IsLoading>
           </div>
         </div>
       </div>
-    </FitScreen>
+    </>
   );
 }
 
 function Body() {
   return (
-    <div className="max-w-3xl mx-auto pb-14 px-4">
-      <div className="pt-14 pb-8">
-        <h2 className='text-2xl font-bold pb-4 text-left font-bold' >
-          하늘인편으로
-          <br />
-          손쉽게 편지를 받아보세요
-        </h2>
-        <p className='text-base text-left'>
-          하늘인편은 편지를 쉽게 받을 수 있는
-          <br />
-          공군 인터넷편지 서비스입니다
-        </p>
-      </div>
+    <div className="max-w-2xl mx-auto pt-8 pb-14 px-4">
       <div className="pb-8">
-        <h2 className='text-2xl font-bold pb-4 text-left font-bold' >
+        <h3 className={styles.title} >
           사용방법
-        </h2>
-        <p className='text-base text-left pb-2.5'>
-          입대 전에 편지함을 만들어 주변 사람들에게 링크를 공유합니다.
+        </h3>
+        <p className={styles.content}>
+          입대 전에 편지함을 만들고 주변 사람들에게 링크를 공유합니다.
         </p>
-        <p className='text-base text-left pb-2.5'>
+        <p className={styles.content}>
           링크를 통해 접속하면 편지를 작성할 수 있습니다. 내용은 최대 1200자까지 작성할 수 있으며 작성한 비밀번호를 통해 이후에 수정 및 삭제가 가능합니다.
         </p>
-        <p className='text-base text-left'>
+        <p className={styles.content}>
           입대 전에도 편지를 받을 수 있으며 내 정보에서 받은 편지 내용을 확인할 수 있습니다.
         </p>
       </div>
       <div className="pb-8">
-        <h2 className='text-2xl font-bold pb-4 text-left font-bold' >
+        <h3 className={styles.title} >
           훈련소 내 편지 발송
-        </h2>
-        <p className='text-base text-left pb-2.5'>
+        </h3>
+        <p className={styles.content}>
           공군 훈련소에서는 훈련 3주차부터 수료 전까지 인쇄된 편지를 받을 수 있습니다.
         </p>
-        <p className='text-base text-left pb-2.5'>
+        <p className={styles.content}>
           하늘인편에서 3주차 이전에 작성한 편지는 모아두었다가 이후부터 순차적으로 발송됩니다.
         </p>
-        <p className='text-base text-left'>
+        <p className={styles.content}>
           일일 편지 전송 제한으로 인해 일부 편지는 추후에 받을 수 있습니다.
         </p>
       </div>
       <div className="pb-8">
-        <h2 className='text-2xl font-bold pb-4 text-left font-bold' >
+        <h3 className={styles.title} >
           주의사항
-        </h2>
-        <p className='text-base text-left'>
+        </h3>
+        <p className={styles.content}>
           <strong> 가입 시 이름 및 생년월일을 정확히 입력해주세요!</strong>
-          <br/>
+          <br />
           훈련병을 식별할 수 없어 편지 발송이 불가능합니다
         </p>
-      </div> 
+      </div>
       <div className="pb-8">
-        <h2 className='text-2xl font-bold pb-4 text-left font-bold' >
+        <h3 className={styles.title} >
           Q&A
-        </h2>
-        <p className='text-base text-left'>
+        </h3>
+        <p className={styles.content}>
           <strong>꼭 입대전에 가입해야 하나요?</strong>
-          <br/>
+          <br />
           아닙니다. 훈련소 내부에서 주말 휴대폰 사용시간을 통해 가입하셔도 되고, 지인에게 대신 부탁해서 가입하셔도 됩니다.
         </p>
-      </div> 
-      
+      </div>
+
     </div>
   );
 }
