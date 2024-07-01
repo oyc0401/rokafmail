3ddf6a0c4925cba0806e0fbf3469597c5a4ff2ff
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-07-01 14:00:38 +0000

    font preload true

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 const notoSansKr = Noto_Sans_KR({
   weight: ["400", "500", "600", "700", "800"],
-  preload: false,
+  preload: true,
   display: "fallback",
+  subsets: ['latin'],
   fallback: ["Noto Sans KR", "sans-serif"],
 });
 
