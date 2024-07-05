b6d62c9209d91976e138ce42c7f28ee21405b288
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-07-05 09:58:17 +0000

    lint

--- src/app/admin/control/user/server.ts
+++ src/app/admin/control/user/server.ts
@@ -2,20 +2,14 @@
 
 import { makeLogger } from "config/winston";
 import { bean } from "src/bean/bean";
-import { Trainee } from "src/service/user/Trainee";
 import { UserService, syncResponseToStr } from "src/service/user/UserService";
 const logger = makeLogger("Control User");
-import { PrismaProfileFactory } from 'src/type/factory';
+import { Trainee } from "src/type/serviceType";
 
 export async function userDoubleCheck(userId: number) {
-
-  const { userRepository } = bean;
-  const user = await userRepository.findById(userId);
-  if (!user) {
-    return;
-  }
-  const trainee = new Trainee(user);
   const userService = new UserService(bean);
+
+  const trainee: Trainee = await userService.getTrainee(userId);
   const response = await userService.updateRokafProfile(userId, trainee);
 
   logger.info(`${trainee.username} (${userId}) | ${syncResponseToStr(response)}`)
--- src/app/admin/info/[username]/server.ts
+++ src/app/admin/info/[username]/server.ts
@@ -4,18 +4,12 @@ import { makeLogger } from "config/winston";
 const logger = makeLogger("Control User");
 import { UserService, syncResponseToStr } from "src/service/user/UserService";
 import { bean } from "src/bean/bean";
-import { Trainee } from "src/service/user/Trainee";
 
 
 export async function userDoubleCheck(userId: number) {
-
-  const { userRepository } = bean;
-  const user = await userRepository.findById(userId);
-  if (!user) {
-    return;
-  }
-  const trainee = new Trainee(user);
   const userService = new UserService(bean);
+
+  const trainee = await userService.getTrainee(userId);
   const status = await userService.updateRokafProfile(userId, trainee);
 
   const log = `${trainee.username} (${userId}) | ${syncResponseToStr(status)}`;
