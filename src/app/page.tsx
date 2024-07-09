9cef1256502c28ca3b6f8e990be376277028fbc4
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-07-09 14:50:11 +0000

    메인페이지 설명 부분 배경색 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,14 +49,8 @@ function TopView() {
         <Link className="submit lg text-lg shadow" href={{ pathname: "/register" }}>
           시작하기
         </Link>
-        <div className="pt-4 pb-5 w-full">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
+        <div className="pt-4 pb-6 w-full">
+          <div className="flex flex-row justify-center"          >
             <IsAuthenticated>
               <a href="/profile" className="text-base underline hover:text-darkaccent">내 정보</a>
             </IsAuthenticated>
@@ -77,58 +71,57 @@ function TopView() {
 
 function Body() {
   return (
-    <div className="max-w-2xl mx-auto pt-8 pb-14 px-4">
-      <div className="pb-8">
-        <h3 className={styles.title} >
-          훈련소 내 편지 발송
-        </h3>
-        <p className={styles.content}>
-          공군 훈련소에서 훈련 3주차부터 수료 전까지 인쇄된 편지를 받을 수 있습니다.
-        </p>
-        <p className={styles.content}>
-          하늘인편에서 훈련 3주차 이전에 작성한 편지는 모아두었다가 이후부터 순차적으로 발송됩니다.
-        </p>
-        <p className={styles.content}>
-          일일 편지 전송 제한으로 인해 일부 편지는 추후에 받아볼 수 있습니다.
-        </p>
+    <div className="bg-[#FAFAFA]  pt-8 pb-14 px-4 ">
+      <div className="max-w-2xl mx-auto">
+        <div className="pb-8">
+          <h3 className={styles.title} >
+            훈련소 내 편지 발송
+          </h3>
+          <p className={styles.content}>
+            공군 훈련소에서 훈련 3주차부터 수료 전까지 인쇄된 편지를 받을 수 있습니다.
+          </p>
+          <p className={styles.content}>
+            하늘인편에서 훈련 3주차 이전에 작성한 편지는 모아두었다가 이후부터 순차적으로 발송됩니다.
+          </p>
+          <p className={styles.content}>
+            일일 편지 전송 제한으로 인해 일부 편지는 추후에 받아볼 수 있습니다.
+          </p>
+        </div>
+        <div className="pb-8">
+          <h3 className={styles.title} >
+            사용방법
+          </h3>
+          <p className={styles.content}>
+            입대 전에 편지함을 만들고 주변 사람들에게 링크를 공유합니다.
+          </p>
+          <p className={styles.content}>
+            링크를 통해 접속하면 편지를 작성할 수 있습니다. 내용은 최대 1200자까지 작성할 수 있으며 작성한 비밀번호를 통해 이후에 수정 및 삭제가 가능합니다.
+          </p>
+          <p className={styles.content}>
+            입대 전에도 편지를 받을 수 있으며 내 정보에서 받은 편지 내용을 확인할 수 있습니다.
+          </p>
+        </div>
+        <div className="pb-8">
+          <h3 className={styles.title} >
+            주의사항
+          </h3>
+          <p className={styles.content}>
+            <strong> 가입 시 이름 및 생년월일을 정확히 입력해주세요!</strong>
+            <br />
+            훈련병을 식별할 수 없어 편지 발송이 불가능합니다
+          </p>
+        </div>
+        <div className="pb-8">
+          <h3 className={styles.title} >
+            Q&A
+          </h3>
+          <p className={styles.content}>
+            <strong>꼭 입대전에 가입해야 하나요?</strong>
+            <br />
+            아닙니다. 훈련소 내부에서 주말 휴대폰 사용시간을 통해 가입하셔도 되고, 지인에게 대신 부탁해서 가입하셔도 됩니다.
+          </p>
+        </div>
       </div>
-
-      <div className="pb-8">
-        <h3 className={styles.title} >
-          사용방법
-        </h3>
-        <p className={styles.content}>
-          입대 전에 편지함을 만들고 주변 사람들에게 링크를 공유합니다.
-        </p>
-        <p className={styles.content}>
-          링크를 통해 접속하면 편지를 작성할 수 있습니다. 내용은 최대 1200자까지 작성할 수 있으며 작성한 비밀번호를 통해 이후에 수정 및 삭제가 가능합니다.
-        </p>
-        <p className={styles.content}>
-          입대 전에도 편지를 받을 수 있으며 내 정보에서 받은 편지 내용을 확인할 수 있습니다.
-        </p>
-      </div>
-
-      <div className="pb-8">
-        <h3 className={styles.title} >
-          주의사항
-        </h3>
-        <p className={styles.content}>
-          <strong> 가입 시 이름 및 생년월일을 정확히 입력해주세요!</strong>
-          <br />
-          훈련병을 식별할 수 없어 편지 발송이 불가능합니다
-        </p>
-      </div>
-      <div className="pb-8">
-        <h3 className={styles.title} >
-          Q&A
-        </h3>
-        <p className={styles.content}>
-          <strong>꼭 입대전에 가입해야 하나요?</strong>
-          <br />
-          아닙니다. 훈련소 내부에서 주말 휴대폰 사용시간을 통해 가입하셔도 되고, 지인에게 대신 부탁해서 가입하셔도 됩니다.
-        </p>
-      </div>
-
     </div>
   );
 }
