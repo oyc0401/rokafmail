d4700bc3e0103a40652facde3bccf9903b0a1ec2
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-07-09 14:30:13 +0000

    리드미

---
+++ linux_command.md
@@ -0,0 +1,3 @@
+## nginx
+로그 생성:$sudo goaccess /var/log/nginx/access.log -o /var/www/html/report.html --log-format=COMBINED 
+
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   deps = [
+    pkgs.mkinitcpio-nfs-utils
     pkgs.utillinux.bin
     pkgs.postgresql
     pkgs.libuuid.lib 
