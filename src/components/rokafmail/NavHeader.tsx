0585404158c25a5d07c54504e5f1a02bb2552e72
yuchan <73932179+oyc0401@users.noreply.github.com>
2024-07-27 08:28:10 +0000

    feat: 앱바 항상 위에 있게 설정. 버튼 투명도 제거, 그냥 색 변하게 함

--- src/components/rokafmail/NavHeader.tsx
+++ src/components/rokafmail/NavHeader.tsx
@@ -12,17 +12,20 @@ const INKLIPQUID = localFont({
 export function NavHeader({ user }) {
   const { username, name, birth, memberSeq, connect } = user;
   return (
-    <nav role="banner" className={'shadow-sm dark:border-b dark:border-gray-800 w-full'}>
-      <header className="max-w-3xl mx-auto px-2 ">
-        <div className="flex justify-between items-center h-14">
-          <a className="flex h-full px-2 mr-5 md:mr-0" href="/">
-            <p className={`${INKLIPQUID.className} ${styles.INKLIPQUID}`}>하늘인편</p>
-          </a>
-          <DropdownButton username={username} name={name} birth={birth} memberSeq={memberSeq} connect={connect}></DropdownButton>
-        </div>
+   <>
+     <div className="h-[56px]"></div>
+     <nav role="banner" className={'fixed top-0 bg-white shadow-sm dark:border-b dark:border-gray-800 w-full'}>
+       <header className="max-w-3xl mx-auto px-2 ">
+         <div className="flex justify-between items-center h-14">
+           <a className="flex h-full px-2 mr-5 md:mr-0" href="/">
+             <p className={`${INKLIPQUID.className} ${styles.INKLIPQUID}`}>하늘인편</p>
+           </a>
+           <DropdownButton username={username} name={name} birth={birth} memberSeq={memberSeq} connect={connect}></DropdownButton>
+         </div>
 
-      </header>
-    </nav>
+       </header>
+     </nav>
+   </>
 
   );
 }
