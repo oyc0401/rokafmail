56e8ce1417cde99b762335285a9cec592640bc1a
yuchan <oyc0401@gmail.com>
2025-12-31 00:29:25 +0900

    prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL + '?pgbouncer=true&connection_limit=1',
-      },
-    },
-  })
+  return new PrismaClient()
 }
 
 declare const globalThis: {
